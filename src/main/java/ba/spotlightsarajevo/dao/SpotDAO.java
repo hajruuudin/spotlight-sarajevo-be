dc79191991b22a5d5fde7a667017db16e6c9919a
hajruuudin <hajruuudin@gmail.com>
2025-04-23 16:41:23 +0200

    Tags added to Shorthand Headlines and Discover

--- src/main/java/ba/spotlightsarajevo/dao/SpotDAO.java
+++ src/main/java/ba/spotlightsarajevo/dao/SpotDAO.java
@@ -1,6 +1,7 @@
 package ba.spotlightsarajevo.dao;
 
 import ba.spotlightsarajevo.dao.entities.SpotEntity;
+import ba.spotlightsarajevo.dao.models.spot.SpotShorthand;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
