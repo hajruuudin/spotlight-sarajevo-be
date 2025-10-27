b9063e11983bdf032478e084050bd0a8f197f33b
Hajrudin Imamovic <hajruuudin@gmail.com>
2025-10-27 19:21:33 +0100

    011: Small Documentation I

--- src/main/java/com/spotlightsarajevo/service/definition/AuthService.java
+++ src/main/java/com/spotlightsarajevo/service/definition/AuthService.java
@@ -4,15 +4,44 @@ import com.spotlightsarajevo.dao.dto.userauth.*;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.http.ResponseEntity;
+import com.spotlightsarajevo.utils.exceptions.AuthExceptions;
+import com.spotlightsarajevo.utils.exceptions.AuthExceptions.SystemLoginException;
 
 import java.io.IOException;
-import java.rmi.NoSuchObjectException;
 import java.security.GeneralSecurityException;
 import java.util.Map;
 
 public interface AuthService {
+    /**
+     * Service method to store google credentials within a session. This method stores the users main credentials
+     * (being email, first and last name, created username and google id, all fields required by the {@link GoogleUserModel}).
+     * The data is stored in a temporary session on the backend, when it is later pulled in the register methods. The method first
+     * verifies if the token is valid, after which is attempts to retrieve the Google Data from Googles servers. If any of the data
+     * is not present, the mentioned exception will be thrown. Upon success, the data should be stored within a newly made session for the user.
+     *
+     * @param payload The payload only consists of a {@link String} labeled <b>idToken</b> which is used to identify the users Google account.
+     *                This is received from the frontend in a String format.
+     * @param session The HTTP session of the user used to add the data temporarily to the backend.
+     *
+     * @throws AuthExceptions.GoogleRegisterException in case the idToken is null, is not present, is invalid or any other potential mishap with Googles GSI api.
+     * @see com.google.api.client.googleapis.auth.oauth2.GoogleIdToken
+     */
     ResponseEntity<Map<String, Object>> storeGoogleCredentials(Map<String, String> payload, HttpSession session) throws GeneralSecurityException, IOException;
+
+    /**
+     * Service method to store system credentials within a session. This method stores the users main credentials
+     * (being email, first and last name, generated username and hashed password, all field required by the {@link SystemUserModel}).
+     * The data is stored in a temporary session when it is later pulled in the register methods for system users. The method
+     * checks if the data is present (if not, throws the exception). Afterward, it hashes the password and stores the user credentials into the session.
+     *
+     * @param payload The data sent from the form on the frontend, essentially a {@link SystemUserModel}
+     * @param session The HTTP session of the user to add the data temporarily to the backend.
+     *
+     * @throws SystemLoginException in case anything is missing, data does not get added or unexpected errors occur within the method.
+     * @see org.springframework.security.crypto.bcrypt.BCrypt
+     */
     ResponseEntity<Map<String, Object>> storeSystemCredentials(SystemUserModel payload, HttpSession session);
+
     ResponseEntity<UserModel> registerByGoogle(GoogleUserModel googleData, PreferencesModel preferencesModel);
     ResponseEntity<UserModel> registerBySystem(SystemUserModel systemData, PreferencesModel preferencesModel);
     ResponseEntity<Map<String, Object>> loginGoogle(Map<String, String> payload, HttpServletResponse servletResponse) throws GeneralSecurityException, IOException;
--- src/main/java/com/spotlightsarajevo/utils/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/spotlightsarajevo/utils/exceptions/GlobalExceptionHandler.java
@@ -10,6 +10,29 @@ import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    /*==== SPOT EXCEPTIONS START ====*/
+    @ExceptionHandler(SpotExceptions.SpotNotFoundException.class)
+    public ResponseEntity<?> handleSpotNotFoundException(SpotExceptions.SpotNotFoundException ex){
+        return buildResponse(HttpStatus.NOT_FOUND, "Spot Find Error", ex.getMessage());
+    }
+
+    @ExceptionHandler(SpotExceptions.SpotCreationException.class)
+    public ResponseEntity<?> handleSpotCreationException(SpotExceptions.SpotCreationException ex){
+        return buildResponse(HttpStatus.INTERNAL_SERVER_ERROR, "Spot Creation Error", ex.getMessage());
+    }
+
+    @ExceptionHandler(SpotExceptions.SpotConflictException.class)
+    public ResponseEntity<?> handleSpotConflictException(SpotExceptions.SpotConflictException ex){
+        return buildResponse(HttpStatus.CONFLICT, "Spot Conflict Error", ex.getMessage());
+    }
+
+    @ExceptionHandler(SpotExceptions.SpotUnauthorizedException.class)
+    public ResponseEntity<?> handleSpotUnauthorisedException(SpotExceptions.SpotUnauthorizedException ex){
+        return buildResponse(HttpStatus.UNAUTHORIZED, "Spot Unauthorised Error", ex.getMessage());
+    }
+    /*==== SPOT EXCEPTIONS END ====*/
+
+    /*==== AUTHENTICATION EXCEPTIONS START ====*/
     @ExceptionHandler(AuthExceptions.UserRegisterException.class)
     public ResponseEntity<?> handleUserDataRegisterException(AuthExceptions.UserRegisterException ex){
         return buildResponse(HttpStatus.BAD_REQUEST, "User Data Registration Error", ex.getMessage());
@@ -29,6 +52,7 @@ public class GlobalExceptionHandler {
     public ResponseEntity<?> handleAuthEndpointException(AuthExceptions.AuthenticationEndpointException ex){
         return buildResponse(HttpStatus.NOT_FOUND, "Authentication Endpoint Error", ex.getMessage());
     }
+    /*==== AUTHENTICATION EXCEPTIONS END ====*/
 
     /* General Exception handler for anything not specified */
     @ExceptionHandler(Exception.class)
